5a4ccb42ba26bd2a66c86fd7166c5fa5319a73cf
User win 10 <levchenko.maksym@gmz31.com>
2019-08-10 01:33:09 +0300

    FINALLY

--- src/main/java/com/example/demo/model/Controller1.java
+++ src/main/java/com/example/demo/model/Controller1.java
@@ -1,12 +1,13 @@
 package com.example.demo.model;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.swing.text.View;
 
-@RestController
-public class Controller {
+@Controller
+public class Controller1 {
     @RequestMapping("/")
     public String getView() {
 
