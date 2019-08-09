5127a0b7aef578140f4f08810f44e9bc5bfcf6ec
User win 10 <levchenko.maksym@gmz31.com>
2019-08-10 02:20:13 +0300

    FINALLY

--- src/main/java/com/example/demo/model/Controller1.java
+++ src/main/java/com/example/demo/model/Controller1.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.swing.text.View;
 
 @Controller
-public class Controller1 {
+public class Controller1
+{
     @RequestMapping("/")
-    public String getView() {
-
+    public String   getView()
+    {
         return "view";
     }
 }
